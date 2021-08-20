e4ab7182ed857aced9501e1aee5a83e9d19d64d6
XuTongle <xutongle@msn.com>
2021-08-20 14:23:14 +0800

    c

--- database/migrations/2019_01_01_161236_create_transaction_charges_table.php
+++ database/migrations/2019_01_01_161236_create_transaction_charges_table.php
@@ -14,35 +14,21 @@ class CreateTransactionChargesTable extends Migration
     public function up()
     {
         Schema::create('transaction_charges', function (Blueprint $table) {
-            $table->string('id',64)->unique();
-            $table->string('trade_channel', 64)->nullable()->comment('付款渠道');
-            $table->string('trade_type', 16)->nullable()->comment('交易类型');
-
-            //订单金额
-            $table->unsignedInteger('total_amount')->comment('订单总金额');
-            $table->string('currency', 3)->default('CNY')->comment('货币类型');
-
-            $table->string('trade_state', 32)->nullable()->comment('交易状态');
-            $table->string('trade_state_desc', 256)->nullable()->comment('交易状态描述');
-            $table->string('bank_type', 16)->nullable()->comment('付款银行');
+            $table->string('id',64)->unique()->comment('付款流水号');
+            $table->string('channel', 64)->nullable()->comment('付款渠道');
+            $table->string('type', 16)->nullable()->comment('交易类型');
             $table->string('transaction_no', 64)->nullable()->comment('支付渠道流水号');
-
-            $table->json('payer')->nullable()->comment('支付者信息');
-
-
-
-
-
-            //场景信息
-            $table->ipAddress('client_ip')->nullable()->comment('用户的客户端IP');
-
+            $table->morphs('order');//订单关联
             $table->string('subject', 256)->nullable()->comment('订单标题');
             $table->string('description', 127)->nullable()->comment('商品描述');
-
-
-            $table->morphs('order');//订单关联
-
-            $table->timestamp('time_expire')->nullable()->comment('交易结束时间');
+            $table->unsignedInteger('total_amount')->comment('订单总金额');
+            $table->string('currency', 3)->default('CNY')->comment('货币类型');
+            $table->string('state', 32)->nullable()->comment('交易状态');
+            $table->string('state_desc', 256)->nullable()->comment('交易状态描述');
+            $table->ipAddress('client_ip')->nullable()->comment('用户的客户端IP');
+            $table->json('payer')->nullable()->comment('支付者信息');
+            $table->json('credential')->nullable()->comment('客户端支付凭证');
+            $table->timestamp('expired_at')->nullable()->comment('交易结束时间');
             $table->timestamps();
             $table->softDeletes();
         });
