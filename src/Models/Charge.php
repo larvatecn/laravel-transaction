1dd723e3f2b64e7e14d5944d56462ed72222395b
Larva Tech <support@larvacent.com>
2021-09-06 23:24:59 +0800

    c

--- src/Models/Charge.php
+++ src/Models/Charge.php
@@ -328,7 +328,7 @@ class Charge extends Model
      * @return bool
      * @throws Exception
      */
-    public function closed(): bool
+    public function close(): bool
     {
         if ($this->state == static::STATE_NOTPAY) {
             $channel = Transaction::getGateway($this->trade_channel);
