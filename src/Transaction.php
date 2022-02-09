2b6e836a7add6cc2a422bd18ac55e155f9cea1eb
XuTongle <xutongle@msn.com>
2022-02-09 19:06:44 +0800

    Laravel 9 特别支持

--- src/Transaction.php
+++ src/Transaction.php
@@ -1,16 +1,8 @@
 <?php
-/**
- * This is NOT a freeware, use is subject to license terms
- * @copyright Copyright (c) 2010-2099 Jinan Larva Information Technology Co., Ltd.
- * @link http://www.larva.com.cn/
- */
 
 declare(strict_types=1);
 /**
  * This is NOT a freeware, use is subject to license terms.
- *
- * @copyright Copyright (c) 2010-2099 Jinan Larva Information Technology Co., Ltd.
- * @link http://www.larva.com.cn/
  */
 namespace Larva\Transaction;
 
